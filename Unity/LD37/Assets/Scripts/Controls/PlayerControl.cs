7f35f66a6047e6824392826495d14eb61c95e302
Matt Van Der Westhuizen <matt.vanderwest@entelect.co.za>
2016-12-11 14:28:52 +0200

    Basic directional animation states implemented prior to implementing weapons.

--- Unity/LD37/Assets/Scripts/Controls/PlayerControl.cs
+++ Unity/LD37/Assets/Scripts/Controls/PlayerControl.cs
@@ -8,7 +8,10 @@ public class PlayerControl : MonoBehaviour
 {
     private Player rewiredPlayer;
     private Rigidbody2D character;
+    private Animator animator;
+
     private Vector2 moveVector;
+    private Vector2 facing = new Vector2(0f, -1.0f);
 
     public int RewiredPlayerId = 0;
     public float MoveSpeed = 3.0f;
@@ -35,6 +38,7 @@ public class PlayerControl : MonoBehaviour
     {
         this.character = GetComponent<Rigidbody2D>();
         this.rewiredPlayer = ReInput.players.GetPlayer(this.RewiredPlayerId);
+        this.animator = GetComponent<Animator>();
     }
 
     private void Update()
@@ -45,6 +49,7 @@ public class PlayerControl : MonoBehaviour
     private void FixedUpdate()
     {
         this.ProcessInput();
+        this.UpdateFacing();
     }
 
     private void GetInput()
@@ -63,5 +68,53 @@ public class PlayerControl : MonoBehaviour
             return;
         }
         this.character.velocity = Vector2.zero;
+    }
+
+    private void UpdateFacing() {
+        if (this.character.velocity.magnitude <= 0) {
+            this.animator.SetFloat("speed", 0.0f);
+            return;
+        }
+
+        this.animator.SetFloat("speed", this.character.velocity.magnitude);
+        if (System.Math.Abs(this.character.velocity.x) >= System.Math.Abs(this.character.velocity.y)) {
+            SetAnimatorBooltIfNotSet("faceSide", true);
+            SetAnimatorBooltIfNotSet("faceUp", false);
+            SetAnimatorBooltIfNotSet("faceDown", false);
+
+            if (this.character.velocity.x < 0) {
+                this.facing = new Vector2(-1.0f, 0.0f);
+
+                Vector3 scale = transform.localScale; ;
+                scale.x = -1;
+                transform.localScale = scale;
+            } else {
+                this.facing = new Vector2(1.0f, 0.0f);
+
+                Vector3 scale = transform.localScale; ;
+                scale.x = 1;
+                transform.localScale = scale;
+            }
+        } else {
+            SetAnimatorBooltIfNotSet("faceSide", false);
+
+            if (this.character.velocity.y < 0) {
+                this.facing = new Vector2(0.0f, -1.0f);
+
+                SetAnimatorBooltIfNotSet("faceUp", false);
+                SetAnimatorBooltIfNotSet("faceDown", true);
+            } else {
+                this.facing = new Vector2(0.0f, 1.0f);
+
+                SetAnimatorBooltIfNotSet("faceUp", true);
+                SetAnimatorBooltIfNotSet("faceDown", false);
+            }
+        }
+    }
+
+    private void SetAnimatorBooltIfNotSet(string name, bool value) {
+        if (this.animator.GetBool(name) != value) {
+            this.animator.SetBool(name, value);
+        }
     }
 }
